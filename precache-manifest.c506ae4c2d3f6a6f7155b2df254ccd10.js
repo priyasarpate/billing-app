88ea92b8a11259315537624f0e2b0a7e30930ce4
Gunpriya Sarpate <sarpatepriya@gmail.com>
2020-09-24 14:14:06 +0530

    Updates

---
+++ precache-manifest.c506ae4c2d3f6a6f7155b2df254ccd10.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42324c0607c6325366be89f9c645e25b",
+    "url": "/billing-app/index.html"
+  },
+  {
+    "revision": "c7f8a911da308c66b351",
+    "url": "/billing-app/static/css/main.74692963.chunk.css"
+  },
+  {
+    "revision": "f3cdd3a5c0129b538b4f",
+    "url": "/billing-app/static/js/2.bcdae8a6.chunk.js"
+  },
+  {
+    "revision": "64f4546471ec1f7db829",
+    "url": "/billing-app/static/js/3.0ce115ed.chunk.js"
+  },
+  {
+    "revision": "b51ed37b171a34c94200ff01ec170480",
+    "url": "/billing-app/static/js/3.0ce115ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae437b3d4630fd66510",
+    "url": "/billing-app/static/js/4.1e9ea7cc.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/billing-app/static/js/4.1e9ea7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f8a911da308c66b351",
+    "url": "/billing-app/static/js/main.9ed14a5e.chunk.js"
+  },
+  {
+    "revision": "c193ea775a99bf1465c4",
+    "url": "/billing-app/static/js/runtime-main.3b47225e.js"
+  }
+]);
