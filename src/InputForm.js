0928f8d6dd0fae939920bfbe873108cbed52210e
Gunpriya Sarpate <sarpatepriya@gmail.com>
2021-06-20 18:27:36 +0530

    responsive design

--- src/InputForm.js
+++ src/InputForm.js
@@ -13,19 +13,19 @@ class InputForm extends Component {
       total: null,
     }
   }
-   
+
 
   handleChangeItem = (event) => {
     this.setState({
       item: event.target.value
-      
+
     })
   }
 
   handleChangePrice = (event) => {
     this.setState({
       price: event.target.value
-    
+
     })
   }
 
@@ -37,63 +37,76 @@ class InputForm extends Component {
   }
 
   handleChangeTotal = (event) => {
-    let {price, GST} = this.state
+    let { price, GST } = this.state
     this.setState({
-      total: parseFloat(price) + parseFloat(GST)  
+      total: parseFloat(price) + parseFloat(GST)
     })
   }
-  Resethandler= ()=>{
-   console.log('from reset handler')
-   this.setState({
-     item: null,
-     price: 0,
-     GST: 0,
-     total: 0
-   })
+  Resethandler = () => {
+    console.log('from reset handler')
+    this.setState({
+      item: null,
+      price: 0,
+      GST: 0,
+      total: 0
+    })
   }
 
   render() {
     return (
-      <React.Fragment>
-     <div className="col-md-4">
-    <Invoice Resethandler ={this.Resethandler} data={this.state}/>
-    </div>  
-<form>
-    <h1>Fill Form</h1>
-    <div className="form-group">
-        <label for="inputAddress">Item:</label>
-        <input type="text" className="form-control" id="inputAddress" placeholder="Item Name"
-            value={this.state.item} onChange={this.handleChangeItem} />
-    </div>
 
-    <div className="form-group">
-        <label for="inputAddress2">Price:</label>
-        <input type="text" className="form-control" id="inputAddress2" placeholder="Price"
-            value=
-            {this.state.price} onChange={this.handleChangePrice}
-        />
-    </div>
-
-    <div className="form-group">
-        <label for="inputAddress">GST:</label>
-        <input type="text" className="form-control" id="inputAddress" placeholder="GST"
-         value={this.state.GST} onFocus={this.handleChangeGST}
-
-        />
-    </div>
-
-    <div className="form-group">
-        <label for="inputAddress2">Total:</label>
-        <input type="text" className="form-control" id="inputAddress2" placeholder="Total"
-         value=
-         {this.state.total} onFocus={this.handleChangeTotal}
-        />
-    </div>
-</form>
-      </React.Fragment>
+      <>
+     <h1 className="h1">Billing App</h1>
+     <h3 className="p">Helpfull For Small Business</h3>
+        <div className="row">
+        
+          <div className="col-md-6">
+            <div className='form-container form-box'>
+              <form>
+                <h1>Form</h1>
 
 
 
+                <div className="form-group">
+                  <label for="inputAddress">Item:</label>
+                  <input type="text" className="form-control" id="inputAddress" placeholder="Item Name"
+                    value={this.state.item} onChange={this.handleChangeItem} />
+                </div>
+
+                <div className="form-group">
+                  <label for="inputAddress2">Price:</label>
+                  <input type="text" className="form-control" id="inputAddress2" placeholder="Price"
+                    value=
+                    {this.state.price} onChange={this.handleChangePrice}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label for="inputAddress">GST:</label>
+                  <input type="text" className="form-control" id="inputAddress" placeholder="GST"
+                    value={this.state.GST} onFocus={this.handleChangeGST}
+
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label for="inputAddress2">Total:</label>
+                  <input type="text" className="form-control" id="inputAddress2" placeholder="Total"
+                    value=
+                    {this.state.total} onFocus={this.handleChangeTotal}
+                  />
+                </div>
+
+
+              </form>
+            </div>
+          </div>
+          <div className="col-md-6">
+          <Invoice Resethandler={this.Resethandler} data={this.state} />
+        </div>
+        </div>
+       
+      </>
 
     )
 
